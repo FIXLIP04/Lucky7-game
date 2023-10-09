dfc5f8334673372f61cfe968aa6bc85a06a2ade7
FIXLIP04 <fixlip04@gmail.com>
2023-10-09 13:49:37 +0300

    adding user input (step 3)

--- src/Lucky7.java
+++ src/Lucky7.java
@@ -1,12 +1,19 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Lucky7 {
     public static void main(String[] args) throws Exception {
         Random random = new Random();
+        Scanner in = new Scanner(System.in);
 
-        int num1, num2, num3;
+        int num1, num2, num3, money;
+        String input;
 
-        for(int money = 5; money >= 1; money--){
+        System.out.println("How much money do you want to play with?");
+        input = in.nextLine();
+        money = Integer.parseInt(input);
+
+        for(; money >= 1; money--){
             num1 = random.nextInt(10)+1;
             num2 = random.nextInt(10)+1;
             num3 = random.nextInt(10)+1;
@@ -28,6 +35,16 @@ public class Lucky7 {
             else{
                 System.out.println("You lost");
             }
+
+            System.out.println("Do you want to play again? You currently have " + (money-1) + "€ left (y/n)");
+            input = in.nextLine();
+            if(input.equals("n") || input.equals("N")){
+                System.exit(0);
+            }
+            else{
+                
+            }
+            
         }
         
     }
