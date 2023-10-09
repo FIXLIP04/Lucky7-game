632ff2a8e79256c16a814749717589658de51309
FIXLIP04 <fixlip04@gmail.com>
2023-10-09 13:30:03 +0300

    adding money system (step 2)

--- src/Lucky7.java
+++ src/Lucky7.java
@@ -5,16 +5,30 @@ public class Lucky7 {
         Random random = new Random();
 
         int num1, num2, num3;
-        
-        num1 = random.nextInt(10)+1;
-        num2 = random.nextInt(10)+1;
-        num3 = random.nextInt(10)+1;
 
-        if(num1 == 7 || num2 == 7 || num3 == 7){
-            System.out.println("You won");
+        for(int money = 5; money >= 1; money--){
+            num1 = random.nextInt(10)+1;
+            num2 = random.nextInt(10)+1;
+            num3 = random.nextInt(10)+1;
+
+            System.out.println(num1 + " " + num2 + " " + num3);
+            
+            if(num1 == 7 && num2 == 7 && num3 == 7){
+                System.out.println("You won 10EUR");
+                money += 10;
+            }
+            else if(num1 == 7 && num2 == 7 || num1 == 7 && num3 == 7 || num2 == 7 && num3 == 7){
+                System.out.println("You won 5EUR");
+                money += 5;
+            }
+            else if(num1 == 7 || num2 == 7 || num3 == 7){
+                System.out.println("You won 3EUR");
+                money += 3;
+            }
+            else{
+                System.out.println("You lost");
+            }
         }
-        else{
-            System.out.println("You lost");
-        }
+        
     }
 }
